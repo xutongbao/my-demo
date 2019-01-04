39562771bed4ab2bbfd37827ec4ecdbf41a9f18c
徐同保 <xutongbao@vackbot.com>
2019-01-04 12:56:36 +0800

    body click

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -6,50 +6,65 @@ class Select extends Component {
     super(props)
     this.state = {
       active: false,
-      selectValue: null
+      selectValue: {}
     }
-  } 	
+  }
   render() {
-  	let { active, selectValue } = this.state
-  	let { value, options } = this.props	
-  	let menu = []
-  	for (let i = 0; i < options.length; i++) {
-  		menu.push(<li className="m-select-list-item" key={i} onClick={this.handleSelectItem.bind(this, options[i])}>{options[i].label} </li>)
-  	}
-  	let selectItem
-  	if (selectValue) {
-  		selectItem = selectValue
-  	} else if (value) {
-  		selectItem = value
-  	} else {
-  		selectItem = {value: '', label: 'Select...'}
-  	}
+    let { active, selectValue } = this.state
+    let { value, options, className } = this.props
+    let menu = []
+    for (let i = 0; i < options.length; i++) {
+      menu.push(<li className={"m-select-list-item " + (selectValue.value === options[i].value ? 'active' : '')} key={i} 
+        onClick={this.handleSelectItem.bind(this, options[i])}>{options[i].label} </li>)
+    }
+    let selectItem
+    if (selectValue.value) {
+      selectItem = selectValue
+    } else if (value) {
+      selectItem = value
+      this.setState({
+        selectValue: value
+      })
+    } else {
+      selectItem = { value: '', label: 'Select...' }
+    }
     return (
-      <div className="m-select-wrap">
-      	<div className="m-select-current" onClick={this.handleClick.bind(this)}>{selectItem.label}</div>
-      	<div className={"m-select-list-wrap " +　(active ? 'active' : '')}>
-      		<ul className="m-select-list">
-      		{menu}
-      		</ul>
-      	</div>
+      <div className={"m-select-wrap " + className}>
+        <div className="m-select-current js-select-current" onClick={this.handleClick.bind(this)}>{selectItem.label}</div>
+        <div className={"m-select-list-wrap " +　(active ? 'active' : '')}>
+          <ul className="m-select-list">
+          {menu}
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
 Object.assign(Select.prototype, {
-	handleClick() {
-		this.setState({
-			active: !this.state.active
-		})
-	},
-	handleSelectItem(item) {
-		this.setState({
-			selectValue: item,
-			active: false
-		})
-		this.props.onChange(item)
-	}
+  componentDidMount() {
+    document.body.addEventListener('click', e => {
+      if (e.target && e.target.className.indexOf('js-select-current') < 0) {
+        this.setState({
+          active: false
+        })        
+        return;
+      }
+      console.log('body');
+    });
+  },
+  handleClick() {
+    this.setState({
+      active: !this.state.active
+    })
+  },
+  handleSelectItem(item) {
+    this.setState({
+      selectValue: item,
+      active: false
+    })
+    this.props.onChange(item)
+  }
 })
 
 export default Select;
